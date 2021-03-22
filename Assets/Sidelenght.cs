75686a0d4c32ffb4187551bb0904ad45b6693414
ToniRajamaki <zingoo16@gmail.com>
2021-03-22 16:24:15 +0200

    Modified UI Script

---
+++ Assets/Sidelenght.cs
@@ -0,0 +1,27 @@
+﻿using ArucoUnity.Objects;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Sidelenght : MonoBehaviour
+{
+    [SerializeField] GameObject MarkerObject;
+    ArucoObject ao;
+    string value;
+    TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ao = MarkerObject.GetComponent<ArucoObject>();
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value = MarkerObject.GetComponent<ArucoMarker>().MarkerSideLength.ToString();
+         text.text = value;
+    }
+}
