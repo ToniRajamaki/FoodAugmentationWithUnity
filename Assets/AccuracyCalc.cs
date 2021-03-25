37b4f668a12cc779da51e66ed087f7e7b75cb7b9
ToniRajamaki <zingoo16@gmail.com>
2021-03-25 11:59:04 +0200

    Added some UI and implemented timed testing

--- Assets/AccuracyCalc.cs
+++ Assets/AccuracyCalc.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AccuracyCalc : MonoBehaviour
@@ -8,29 +9,68 @@ public class AccuracyCalc : MonoBehaviour
     [SerializeField] GameObject ao;
     int found;
     int totalFrames;
+    float accuracy;
+    [SerializeField] int testingTime;
+    bool stillTesting;
+
+    [SerializeField] GameObject framesObject;
+    TextMeshProUGUI totalFramesText;
+
+    [SerializeField] GameObject foundObject;
+    TextMeshProUGUI foundText;
+
+    [SerializeField] GameObject accuracyObject;
+    TextMeshProUGUI accuracyText;
     // Start is called before the first frame update
     void Start()
     {
+        stillTesting = false;
+        totalFramesText = framesObject.GetComponent<TextMeshProUGUI>();
+        foundText = foundObject.GetComponent<TextMeshProUGUI>();
+        accuracyText = accuracyObject.GetComponent<TextMeshProUGUI>();
         found = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ao.active == true)
+        if (Input.GetKeyDown("space"))
         {
-            found++;
+            stillTesting = true;
+            Invoke("StopTesting", testingTime);
         }
 
-        if (found > 0)
-        {
-            totalFrames++;
-        }
 
-        if(totalFrames%50 == 0)
+        if (stillTesting)
         {
-            print("found:"+found);
-            print("total frames:" + totalFrames);
+            if (ao.active == true)
+            {
+                stillTesting = true;
+                found++;
+            }
+
+            if (found > 0)
+            {
+                totalFrames++;
+            }
+
+
+            foundText.text = found.ToString();
+            totalFramesText.text = totalFrames.ToString();
+            if (totalFrames > 0)
+            {
+                accuracy = (100f * found / totalFrames);
+                accuracyText.text = accuracy.ToString() + '%';
+            }
+            return;
         }
     }
+
+
+    public void StopTesting()
+    {
+        stillTesting = false;
+        print(accuracy);
+    }
 }
+
